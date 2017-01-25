99e4ce8cdbcf2b9f84b447c10a79eb7dde31df0e
Jose Eguiguren <pepe.96.jose@gmail.com>
2017-01-25 14:10:39 -0500

    Bug fixes

--- src/controller/Parser.java
+++ src/controller/Parser.java
@@ -24,80 +24,87 @@ public class Parser {
     }
 
     public void parse(String instruction) throws ParseException {
-        //Ignore comments
-        if (!instruction.startsWith("//")) {
+        //Ignore blanks
+        if (!instruction.isEmpty()) {
+            //Ignore comments
+            if (!instruction.startsWith("//")) {
 
-            if (instruction.toLowerCase().equals("clear")) {
-                ConsoleManager.clear();
-                Computer.processCounter--;
-            } else if (instruction.toLowerCase().equals("show")) {
-                log();
-            } else {
-                if (instruction.contains("-")) {
-                    String[] split = instruction.split("-");
+                if (instruction.toLowerCase().equals("clear")) {
+                    ConsoleManager.clear();
+                    Computer.processCounter--;
+                } else if (instruction.toLowerCase().equals("show")) {
+                    log();
+                } else {
+                    if (instruction.contains("-")) {
+                        String[] split = instruction.split("-");
 
-                    if (split.length == 2) {
-                        //Split instruction
-                        mInstruction = Integer.parseInt(split[0]);
-                        mMemoryPos = Integer.parseInt(split[1]);
+                        if (split.length == 2) {
+                            //Split instruction
+                            mInstruction = Integer.parseInt(split[0]);
+                            mMemoryPos = Integer.parseInt(split[1]);
 
-                        //Check instruction domain
-                        if (mInstruction > 0 && mInstruction <= 21) {
-                            //Check memory pos
-                            if (mMemoryPos >= 900 && mMemoryPos <= 914) {
+                            //Check instruction domain
+                            if (mInstruction > 0 && mInstruction <= 21) {
+                                //Check memory pos
+                                if (mMemoryPos >= 900 && mMemoryPos <= 914) {
 
-                                switch (mInstruction) {
-                                    case 1:
-                                        instr1();
-                                        log();
-                                        break;
-                                    case 2:
-                                        instr2();
-                                        log();
-                                        break;
-                                    case 5:
-                                        instr5();
-                                        log();
-                                        break;
-                                    case 6:
-                                        instr6();
-                                        log();
-                                    case 7:
-                                        instr7();
-                                        log();
-                                    case 8:
-                                        instr8();
-                                        log();
-                                    case 16:
-                                        instr16();
-                                        log();
-                                    case 17:
-                                        instr17();
-                                        log();
-                                    default:
-                                        ConsoleManager.printToConsole(getErrorMessage() + "Instruction does not exist");
-                                        throw new ParseException("Instruction does not exist", 0);
+                                    switch (mInstruction) {
+                                        case 1:
+                                            instr1();
+                                            log();
+                                            break;
+                                        case 2:
+                                            instr2();
+                                            log();
+                                            break;
+                                        case 5:
+                                            instr5();
+                                            log();
+                                            break;
+                                        case 6:
+                                            instr6();
+                                            log();
+                                            break;
+                                        case 7:
+                                            instr7();
+                                            log();
+                                            break;
+                                        case 8:
+                                            instr8();
+                                            log();
+                                            break;
+                                        case 16:
+                                            instr16();
+                                            log();
+                                            break;
+                                        case 17:
+                                            instr17();
+                                            log();
+                                            break;
+                                        default:
+                                            ConsoleManager.printToConsole(getErrorMessage() + "Instruction does not exist");
+                                            throw new ParseException("Instruction does not exist", 0);
+                                    }
+                                } else {
+                                    ConsoleManager.printToConsole(getErrorMessage() + "Memory position does not exist");
+                                    throw new ParseException("Memory position does not exist", 0);
                                 }
                             } else {
-                                ConsoleManager.printToConsole(getErrorMessage() + "Memory position does not exist");
-                                throw new ParseException("Memory position does not exist", 0);
+                                ConsoleManager.printToConsole(getErrorMessage() + "Instruction does not exist");
+                                throw new ParseException("Instruction does not exist", 0);
                             }
-                        } else {
-                            ConsoleManager.printToConsole(getErrorMessage() + "Instruction does not exist");
-                            throw new ParseException("Instruction does not exist", 0);
-                        }
 
+                        } else {
+                            ConsoleManager.printToConsole(getErrorMessage() + "Incorrect number of arguments");
+                            throw new ParseException("Incorrect number of arguments", 0);
+                        }
                     } else {
-                        ConsoleManager.printToConsole(getErrorMessage() + "Incorrect number of arguments");
-                        throw new ParseException("Incorrect number of arguments", 0);
+                        ConsoleManager.printToConsole(getErrorMessage() + "Wrong syntax");
+                        throw new ParseException("Wrong syntax", 0);
                     }
-                } else {
-                    ConsoleManager.printToConsole(getErrorMessage() + "Wrong syntax");
-                    throw new ParseException("Wrong syntax", 0);
                 }
             }
         }
-
     }
 
     private String getErrorMessage() {
